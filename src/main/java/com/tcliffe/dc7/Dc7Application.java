cc1040dc3180b979a540b44ba469e2c2af9c0efc
vickdw <dvick@boundlessgeo.com>
2018-04-12 21:06:54 -0500

    project cleanup/rebuild. initial commit.

---
+++ src/main/java/com/tcliffe/dc7/Dc7Application.java
@@ -0,0 +1,12 @@
+package com.tcliffe.dc7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dc7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dc7Application.class, args);
+	}
+}
